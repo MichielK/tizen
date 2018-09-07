20d21dbcdde32be49f2b4e44981797e2f17c93c7
Michiel Korthuis <michiel.korthuis@topicus.nl>
2018-09-07 15:22:24 +0200

    werkende requests naar HomeWizard

--- js/app.js
+++ js/app.js
@@ -26,16 +26,41 @@
 
 	function onClick(event) {
 		var target = event.target;
-		
+
+		// controle of juiste element "aangeraakt" is
 		if (!target.classList.contains("ui-selector-indicator")) {
 			return;
 		}
 
 		target = document.getElementsByClassName("ui-item-active")[0];
-		console.log(target);
+
+		var url = 'http://' + ipadres + '/' + wachtwoord + '/sw/' + target.getAttribute("data-id") + '/';
 		
-		console.log('Clicked title: ' + target.getAttribute("data-title"));
-		console.log('Clicked id: ' + target.getAttribute("data-id"));
+		if (target.classList.contains('yellow')) {
+			url = url + 'off';
+			target.classList.remove('yellow');
+			console.log(target.getAttribute("data-title") + ' uitgezet');
+		} else {
+			url = url + 'on';
+			target.classList.add('yellow');
+			console.log(target.getAttribute("data-title") + ' aangezet');
+		}
+
+		var xmlhttp = new XMLHttpRequest();
+
+		// xmlhttp.onreadystatechange = function() {
+		// if (xmlhttp.readyState == xmlhttp.DONE) {
+		// console.log(xmlhttp.responseText);
+		// } else {
+		// console.log(xmlhttp.statusText);
+		// }
+		// };
+		// xmlhttp.onerror = function(e) {
+		// console.log("onerror: " + xmlhttp.statusText);
+		// };
+
+		xmlhttp.open("GET", url);
+		xmlhttp.send();
 		return;
 	}
 	page.addEventListener("pagebeforeshow", function() {
@@ -56,15 +81,14 @@
 	var listData = JSON_DATA_STATUS.response.switches;
 	for (var i = 0; i < listData.length; i++) {
 		var dataElement = listData[i];
-		if(dataElement.type === 'somfy')
-		{
+		if (dataElement.type === 'somfy') {
 			continue;
 		}
 
 		var naam = dataElement.name;
 		var lowercaseNaam = naam.toLowerCase();
-		
-		var id = dataElement.id; // TODO
+
+		var id = dataElement.id;
 		var status = dataElement.status;
 
 		var html = '<div class="ui-item ';
@@ -76,15 +100,14 @@
 			html = html + 'switch';
 		} else if (lowercaseNaam.search('stekkerdoos') > -1) {
 			html = html + 'socket';
-		}
-		else {
+		} else {
 			console.log("Kon geen icoontje bepalen: " + naam);
 		}
 
 		if (status === 'on') {
-			html = html + '-aan-icon yellow" data-title="';
+			html = html + '-icon yellow" data-title="';
 		} else {
-			html = html + '-uit-icon" data-title="';
+			html = html + '-icon" data-title="';
 		}
 
 		html = html + naam + '" data-id="' + id + '"></div>';
